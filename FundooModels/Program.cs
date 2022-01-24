062021d0bddeedf53d64e3785bb1fe23e9c32b5d
Shreesh Pandey <shvspandey@gmail.com>
2022-01-24 18:37:10 +0530

    Structure of the project Added in the master branch

---
+++ FundooModels/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FundooModels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
